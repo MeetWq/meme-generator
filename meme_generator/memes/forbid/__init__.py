d36224ae78bd8c8b0b6f936ecc7b39fe39642d1d
Plasma4004 <Plasma_mc@outlook.com>
2023-03-12 15:26:12 +0800

    fix forbid

--- meme_generator/memes/forbid/__init__.py
+++ meme_generator/memes/forbid/__init__.py
@@ -4,14 +4,16 @@ from typing import List
 from pil_utils import BuildImage
 
 from meme_generator import add_meme
+from meme_generator.utils import make_jpg_or_gif
 
 img_dir = Path(__file__).parent / "images"
 
-
 def forbid (images: List[BuildImage], texts, args):
     frame = BuildImage.open(img_dir / "0.png")
-    frame.copy().paste(img.resize((304, 324), keep_ratio=True), (0, 0), below=True)
-    return frame.save_jpg()
 
+    def make(img: BuildImage) -> BuildImage:
+        return frame.copy().paste(img.resize((304, 324), keep_ratio=True), (0, 0), below=True)
+    
+    return make_jpg_or_gif(images[0], make)
 
 add_meme("forbid", forbid, min_images=1, max_images=1, keywords=["禁止", "禁"])
