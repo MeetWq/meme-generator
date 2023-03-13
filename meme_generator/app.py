075b5183559d365cea0962128255d74853c39a31
meetwq <meetwq@gmail.com>
2023-03-13 17:01:14 +0800

    add parse_args api

--- meme_generator/app.py
+++ meme_generator/app.py
@@ -1,3 +1,5 @@
+import copy
+from argparse import ArgumentParser
 from typing import Any, Dict, List, Optional
 
 import filetype
@@ -6,11 +8,19 @@ from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, ValidationError
 
 from meme_generator.config import meme_config
-from meme_generator.exception import MemeGeneratorException, NoSuchMeme
+from meme_generator.exception import ArgParserExit, MemeGeneratorException, NoSuchMeme
 from meme_generator.manager import get_meme, get_meme_keys, get_memes
 from meme_generator.meme import Meme, MemeArgsModel
 
 app = FastAPI()
+
+
+class MemeArgsResponse(BaseModel):
+    name: str
+    type: str
+    description: Optional[str] = None
+    default: Optional[Any] = None
+    enum: Optional[List[Any]] = None
 
 
 class MemeParamsResponse(BaseModel):
@@ -19,7 +29,7 @@ class MemeParamsResponse(BaseModel):
     min_texts: int
     max_texts: int
     default_texts: List[str]
-    args: Dict[str, Any]
+    args: List[MemeArgsResponse]
 
 
 class MemeInfoResponse(BaseModel):
@@ -88,6 +98,10 @@ def register_routers():
             if meme.params_type.args_type
             else MemeArgsModel
         )
+        properties: Dict[str, Dict[str, Any]] = args_model.schema().get(
+            "properties", {}
+        )
+        properties.pop("user_infos")
         return MemeInfoResponse(
             key=meme.key,
             keywords=meme.keywords,
@@ -98,7 +112,16 @@ def register_routers():
                 min_texts=meme.params_type.min_texts,
                 max_texts=meme.params_type.max_texts,
                 default_texts=meme.params_type.default_texts,
-                args=args_model().dict(),
+                args=[
+                    MemeArgsResponse(
+                        name=name,
+                        type=info.get("type", ""),
+                        description=info.get("description"),
+                        default=info.get("default"),
+                        enum=info.get("enum"),
+                    )
+                    for name, info in properties.items()
+                ],
             ),
         )
 
@@ -113,6 +136,23 @@ def register_routers():
         content = result.getvalue()
         media_type = str(filetype.guess_mime(content)) or "text/plain"
         return Response(content=content, media_type=media_type)
+
+    @app.post("/memes/{key}/parse_args")
+    async def _(key: str, args: List[str] = []):
+        try:
+            meme = get_meme(key)
+            parser = (
+                copy.deepcopy(meme.params_type.args_type.parser)
+                if meme.params_type.args_type
+                else ArgumentParser()
+            )
+            parser.add_argument("texts", nargs="*", default=[])
+            try:
+                return vars(parser.parse_args(args))
+            except:
+                raise ArgParserExit(meme.key)
+        except MemeGeneratorException as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
     for meme in sorted(get_memes(), key=lambda meme: meme.key):
         register_router(meme)
