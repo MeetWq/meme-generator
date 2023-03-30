9a26af0bcf67e30b0375f48143dd6a2e3a460b19
meetwq <meetwq@gmail.com>
2023-03-30 14:24:21 +0800

    Meme: capoo_say support multiple texts

--- meme_generator/memes/capoo_say/__init__.py
+++ meme_generator/memes/capoo_say/__init__.py
@@ -11,32 +11,32 @@ from meme_generator.utils import save_gif
 img_dir = Path(__file__).parent / "images"
 
 
-def capoo_say(images, texts: List[str], args):
-    text = texts[0]
+def capoo_say_one_loop(text: str) -> List[IMG]:
     text_frame = BuildImage.new("RGBA", (80, 80))
     try:
         text_frame.draw_text(
             (0, 0, 80, 80),
             text,
             max_fontsize=80,
-            min_fontsize=30,
+            min_fontsize=20,
             allow_wrap=True,
             fontname="FZKaTong-M19S",
+            lines_align="center",
         )
     except ValueError:
         raise TextOverLength(text)
 
     params = [
-        (80, 80, 43, 30, 0),
-        (78, 78, 44, 30, 0),
-        (78, 78, 44, 29, 0),
-        None,
         None,
         None,
         None,
         (45, 45, 74, 112, 25),
         (73, 73, 41, 42, 17),
         (80, 80, 43, 36, 0),
+        (80, 80, 43, 30, 0),
+        (78, 78, 44, 30, 0),
+        (78, 78, 44, 29, 0),
+        None,
     ]
 
     frames: List[IMG] = []
@@ -49,14 +49,19 @@ def capoo_say(images, texts: List[str], args):
                 text_frame.resize((x, y)).rotate(angle, expand=True), (w, h), alpha=True
             )
         frames.append(frame.image)
-    return save_gif(frames, 0.11)
+    return frames
+
+
+def capoo_say(images, texts: List[str], args):
+    frames = sum([capoo_say_one_loop(text) for text in texts], [])
+    return save_gif(frames, 0.1)
 
 
 add_meme(
     "capoo_say",
     capoo_say,
     min_texts=1,
-    max_texts=1,
+    max_texts=10,
     default_texts=["寄"],
     keywords=["咖波说"],
 )
