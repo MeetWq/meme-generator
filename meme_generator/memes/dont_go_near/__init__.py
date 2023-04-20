872de8694ee6fe2092bf8898a8ff8c2b6ae0ca15
meetwq <meetwq@gmail.com>
2023-04-20 17:30:53 +0800

    rename dont_touch to dont_go_near

--- meme_generator/memes/dont_go_near/__init__.py
+++ meme_generator/memes/dont_go_near/__init__.py
@@ -9,7 +9,7 @@ from meme_generator.utils import make_jpg_or_gif
 img_dir = Path(__file__).parent / "images"
 
 
-def dont_touch(images: List[BuildImage], texts, args):
+def dont_go_near(images: List[BuildImage], texts, args):
     frame = BuildImage.open(img_dir / "0.png")
 
     def make(img: BuildImage) -> BuildImage:
@@ -19,4 +19,4 @@ def dont_touch(images: List[BuildImage], texts, args):
     return make_jpg_or_gif(images[0], make)
 
 
-add_meme("dont_touch", dont_touch, min_images=1, max_images=1, keywords=["不要靠近"])
+add_meme("dont_go_near", dont_go_near, min_images=1, max_images=1, keywords=["不要靠近"])
