8edeff5dbe70b0239a208b02ecd51f61be0cf67c
meetwq <meetwq@gmail.com>
2023-04-27 14:22:41 +0800

    adjust

--- meme_generator/memes/note_for_leave/__init__.py
+++ meme_generator/memes/note_for_leave/__init__.py
@@ -1,8 +1,8 @@
-import datetime
-import re
+from datetime import datetime
 from typing import List
 
-from pil_utils import BuildImage
+import dateparser
+from pil_utils import BuildImage, Text2Image
 from pydantic import Field
 
 from meme_generator import MemeArgsModel, MemeArgsParser, MemeArgsType, add_meme
@@ -11,13 +11,7 @@ from meme_generator.exception import TextOverLength
 help = "指定时间"
 
 parser = MemeArgsParser()
-parser.add_argument(
-    "-t",
-    "--time",
-    type=str,
-    default="",
-    help=help,
-)
+parser.add_argument("--time", type=str, default="", help=help)
 
 
 class Model(MemeArgsModel):
@@ -25,60 +19,44 @@ class Model(MemeArgsModel):
 
 
 def note_for_leave(images: List[BuildImage], texts: List[str], args: Model):
-    time_re = r"\d{4}\.\d{1,2}\.\d{1,2}"
-    time = (
-        datetime.datetime.now().strftime("%Y.%m.%d")
-        if args.time == "" or not re.match(time_re, args.time)
-        else args.time
-    )
-    time_ls = time.split(".")
-    name = (texts[0] if texts else "") or (
-        args.user_infos[-1].name if args.user_infos else ""
-    )
+    time = datetime.now()
+    if args.time and (parsed_time := dateparser.parse(args.time)):
+        time = parsed_time
+    name = texts[0] if texts else (args.user_infos[-1].name if args.user_infos else "")
     img = (
         images[0]
         .convert("RGBA")
-        .resize((410, 400), keep_ratio=True, inside=True, bg_color="white")
+        .resize((450, 400), keep_ratio=True, inside=True, bg_color="white")
     )
-    frame = BuildImage.new("RGBA", (830, 1024), "white")
+
+    frame = BuildImage.new("RGBA", (800, 950), "white")
     frame.draw_text(
-        (270, 78, 589, 180),
+        (40, 20, 760, 180),
         text="请假条",
-        max_fontsize=90,
-        min_fontsize=50,
+        weight="bold",
+        max_fontsize=100,
+        min_fontsize=80,
     )
-    try:
-        frame.draw_text(
-            (113, 249, 466, 310),
-            text="本人" + name + "因",
-            max_fontsize=50,
-            min_fontsize=21,
-            allow_wrap=True,
-        )
-    except TextOverLength:
+    frame.draw_text((40, 200), "本人", fontsize=50)
+    name_width = Text2Image.from_text(name, fontsize=50).width
+    if (name_width) > 800:
         raise TextOverLength(name)
+    name_width = min(450, max(150, name_width)) + 50
     frame.draw_text(
-        (124, 431, 335, 531),
-        text="想玩",
-        max_fontsize=90,
-        min_fontsize=70,
-        fill="red",
+        (150, 200, 150 + name_width, 265), text=name, max_fontsize=50, min_fontsize=20
     )
-    frame.paste(img, (338, 311))
-    frame.draw_text(
-        (90, 709, 780, 770),
-        text=f"于{time_ls[0]}年{time_ls[1]}月{time_ls[2]}日请假一天,",
+    frame.draw_line((150, 260, 150 + name_width, 260), fill="black", width=4)
+    frame.draw_text((160 + name_width, 200), "因", fontsize=50)
+    frame.draw_text((70, 390), text="想玩", fontsize=90, fill="red")
+    frame.paste(img, (300, 290))
+    frame.draw_bbcode_text(
+        (40, 700, 760, 800),
+        text=f"于[u] {time.year} [/u]年[u] {time.month} [/u]月[u] {time.day} [/u]日请假一天,",
         max_fontsize=50,
         min_fontsize=30,
         halign="left",
     )
-    frame.draw_text(
-        (90, 793, 780, 875),
-        text="望领导批准！！！",
-        max_fontsize=75,
-        min_fontsize=40,
-        halign="left",
-    )
+    frame.draw_text((40, 800), text="望领导批准！！！", fontsize=75, weight="bold")
     return frame.save_jpg()
 
 
@@ -89,6 +67,6 @@ add_meme(
     max_images=1,
     min_texts=0,
     max_texts=1,
-    args_type=MemeArgsType(parser, Model, [Model(time="1145.1.4")]),
+    args_type=MemeArgsType(parser, Model),
     keywords=["请假条"],
 )
