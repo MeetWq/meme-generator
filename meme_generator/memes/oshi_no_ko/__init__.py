a1061d0cd1fd77e6afd745185084fe7b1154cba3
jcjrobert <jcjrobbie@gmail.com>
2023-06-02 00:02:19 +0800

    adjust oshi_no_ko

--- meme_generator/memes/oshi_no_ko/__init__.py
+++ meme_generator/memes/oshi_no_ko/__init__.py
@@ -2,44 +2,45 @@ from pathlib import Path
 from typing import List
 
 from pil_utils import BuildImage
+from pydantic import Field
 
-from meme_generator import MemeArgsModel, add_meme
+from meme_generator import MemeArgsModel, MemeArgsParser, MemeArgsType, add_meme
+from meme_generator.utils import make_png_or_gif
 from meme_generator.exception import TextOverLength
 
 img_dir = Path(__file__).parent / "images"
 
+help = "指定名字"
 
-def OSHI_NO_KO(images: List[BuildImage], texts, args: MemeArgsModel):
-    img = images[0]
-    fontsize = 76
-    if img.width <= img.height:
-        img = img.convert("RGBA").resize_width(691)
-    else:
-        img = img.convert("RGBA").resize_height(691)
-    print(texts)
-    if texts:
-        text = texts[0]
-    elif args.user_infos:
-        text = args.user_infos[0].name
-    else:
-        text = "网友"
+parser = MemeArgsParser()
+parser.add_argument("-n", "--name", type=str, default="", help=help)
 
-    if len(text) > 2:
-        text = text[0:2]
-    elif len(text) == 1:
-        fontsize = 152
+
+class Model(MemeArgsModel):
+    name: str = Field("", description=help)
+
+
+def OSHI_NO_KO(images: List[BuildImage], texts, args: Model):
+    name = args.name or "网友"
+
     frame = BuildImage.open(img_dir / "0.png")
-    frame.paste(img, (0, 0), alpha=True, below=True)
-    frame.draw_text(
-        (433, 28, 589, 184),
-        text,
-        fontsize=fontsize,
-        max_fontsize=160,
-        min_fontsize=60,
-        stroke_ratio=0.1,
-        stroke_fill="white",
-    )
-    return frame.save_jpg()
+    try:
+        frame.draw_text(
+            (430, 28, 614, 175),
+            name,
+            max_fontsize=200,
+            min_fontsize=76,
+            stroke_ratio=0.05,
+            stroke_fill="white",
+        )
+    except ValueError:
+        raise TextOverLength(name)
+
+    def make(img: BuildImage) -> BuildImage:
+        img = img.convert("RGBA").resize((691, 691), keep_ratio=True)
+        return frame.copy().paste(img, (0, 0), alpha=True, below=True)
+
+    return make_png_or_gif(images[0], make)
 
 
 add_meme(
@@ -49,5 +50,6 @@ add_meme(
     max_images=1,
     max_texts=1,
     default_texts=["网友"],
+    args_type=MemeArgsType(parser, Model),
     keywords=["我推的网友"],
 )
