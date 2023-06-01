e041d9cf0f1c21b6886f4d5cf9fd802b34781e51
十年 <105840558+shinianj@users.noreply.github.com>
2023-06-01 19:39:33 +0800

    Add files via upload

--- meme_generator/memes/oshi_no_ko/__init__.py
+++ meme_generator/memes/oshi_no_ko/__init__.py
@@ -2,6 +2,7 @@ from pil_utils import BuildImage
 from typing import List
 
 from meme_generator import add_meme,MemeArgsModel
+from meme_generator.exception import TextOverLength
 
 from pathlib import Path
 
@@ -15,9 +16,9 @@ def OSHI_NO_KO(images: List[BuildImage], texts, args: MemeArgsModel):
         text = texts[0]
     else:
         text = args.user_infos[0].name
-    if len(text)>2:
-        text = text[0:2]
     
+    if len(text)>2:
+            text = text[0:2]
     frame = BuildImage.open(img_dir/'0.png')
     frame.paste(img, (0,0), alpha = True,below = True)
     frame.draw_text((433,28,589,184),text,fontsize=76, max_fontsize=80, min_fontsize=60, stroke_ratio = 0.1,stroke_fill = 'white')
