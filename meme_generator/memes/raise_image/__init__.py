fd03bd316190c7ec10389d2df2529f64f4894ca8
student_2333 <lgc2333@126.com>
2023-08-08 21:34:11 +0800

    support gif & add white bg for transparent imgs

--- meme_generator/memes/raise_image/__init__.py
+++ meme_generator/memes/raise_image/__init__.py
@@ -3,21 +3,24 @@ from pathlib import Path
 from typing import List
 
 from meme_generator import add_meme
+from meme_generator.utils import make_jpg_or_gif
 from pil_utils import BuildImage
 
 img_dir = Path(__file__).parent / "images"
 
 
-def raise_image(images: List[BuildImage], texts, args) -> BytesIO:
-    inner_img = images[0]
-    img = BuildImage.open(img_dir / "raise_image.png")
-    img = img.paste(
-        inner_img.resize((599, 386), keep_ratio=True),
-        (134, 91),
-        alpha=True,
-        below=True,
-    )
-    return img.save_png()
+def raise_image(images: List[BuildImage], texts, args) -> BytesIO:  # noqa: ARG001
+    inner_size = (599, 386)
+    paste_pos = (134, 91)
+
+    bg = BuildImage.open(img_dir / "raise_image.png")
+
+    def make_frame(img: BuildImage) -> BuildImage:
+        img = img.resize(inner_size, keep_ratio=True)
+        inner_frame = BuildImage.new("RGBA", inner_size, "white").paste(img, alpha=True)
+        return bg.copy().paste(inner_frame, paste_pos, alpha=True, below=True)
+
+    return make_jpg_or_gif(images[0], make_frame)
 
 
 add_meme(
