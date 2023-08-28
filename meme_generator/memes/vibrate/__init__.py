e8b111ffbd1c2db65b9fa6ae58d574001213de94
meetwq <meetwq@gmail.com>
2023-08-28 16:31:22 +0800

    format code

--- meme_generator/memes/vibrate/__init__.py
+++ meme_generator/memes/vibrate/__init__.py
@@ -10,20 +10,23 @@ img_dir = Path(__file__).parent / "images"
 
 
 def vibrate(images: List[BuildImage], texts, args):
-    
     def maker(i: int) -> Maker:
         def make(img: BuildImage) -> BuildImage:
             img = img.convert("RGBA").square()
             w = img.size[0]
             locs = [
-                (0, 0), (w//25, w//25), (w//50, w//50), (0, w//25), (w//25, 0)
+                (0, 0),
+                (w // 25, w // 25),
+                (w // 50, w // 50),
+                (0, w // 25),
+                (w // 25, 0),
             ]
-            frame = BuildImage.new("RGBA", (w+w//25, w+w//25), "white")
+            frame = BuildImage.new("RGBA", (w + w // 25, w + w // 25), "white")
             frame.paste(img, locs[i], alpha=True)
             return frame
 
         return make
-    
+
     return make_gif_or_combined_gif(
         images[0], maker, 5, 0.05, FrameAlignPolicy.extend_loop
     )
