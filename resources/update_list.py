438e3fa845b54883966ecdf2161930aaf45be7a0
meetwq <meetwq@gmail.com>
2023-12-03 21:08:26 +0800

    resource_list.json use indent 2

--- resources/update_list.py
+++ resources/update_list.py
@@ -19,7 +19,7 @@ def update():
         )
     resource_list.sort(key=lambda i: i["path"])
     with open(dir_path / "resource_list.json", "w", encoding="utf-8") as f:
-        json.dump(resource_list, f, ensure_ascii=False, indent=4)
+        json.dump(resource_list, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
