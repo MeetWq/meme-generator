395ccd2bb61ccf3ef0c271092bbacc327a7c3a2b
meetwq <meetwq@gmail.com>
2024-01-23 21:00:37 +0800

    adjust

--- meme_generator/memes/frieren_take/__init__.py
+++ meme_generator/memes/frieren_take/__init__.py
@@ -11,11 +11,10 @@ from meme_generator.utils import make_jpg_or_gif
 img_dir = Path(__file__).parent / "images"
 
 help = "是否使用默认文字"
+default_text = "所谓的男人啊，只要送他们这种东西就会很开心"
 
 parser = MemeArgsParser(prefix_chars="-/")
-parser.add_argument(
-    "-default", "/默认", "-默认", "/d", "-d", action="store_true", help=help
-)
+parser.add_argument("-d", "--default", "/默认", action="store_true", help=help)
 
 
 class Model(MemeArgsModel):
@@ -24,7 +23,7 @@ class Model(MemeArgsModel):
 
 def frieren_take(images: List[BuildImage], texts: List[str], args: Model):
     frame = BuildImage.open(img_dir / "0.png")
-    text = "所谓的男人啊，只要送他们这种东西就会很开心" if args.default else texts[0] if texts else None
+    text = default_text if args.default else texts[0] if texts else None
     if text:
         try:
             frame.draw_text(
@@ -52,7 +51,7 @@ add_meme(
     min_images=1,
     max_images=1,
     max_texts=1,
-    default_texts=["所谓的男人啊，只要送他们这种东西就会很开心"],
+    default_texts=[default_text],
     args_type=MemeArgsType(parser, Model, [Model(default=False), Model(default=True)]),
     keywords=["芙莉莲拿"],
 )
