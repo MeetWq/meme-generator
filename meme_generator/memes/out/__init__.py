887a282dc3e282a2f7fae1666cdff87ad9700143
ikechan8370 <geyinchibuaa@gmail.com>
2024-04-26 00:39:01 +0800

    fix: rename function

--- meme_generator/memes/out/__init__.py
+++ meme_generator/memes/out/__init__.py
@@ -9,7 +9,7 @@ from meme_generator.utils import make_jpg_or_gif
 img_dir = Path(__file__).parent / "images"
 
 
-def need(images: List[BuildImage], texts, args):
+def out(images: List[BuildImage], texts, args):
     frame = BuildImage.open(img_dir / "out.png")
 
     def make(img: BuildImage) -> BuildImage:
@@ -27,4 +27,4 @@ def need(images: List[BuildImage], texts, args):
     return make_jpg_or_gif(images[0], make, keep_transparency=True)
 
 
-add_meme("out", need, min_images=1, max_images=1, keywords=["out"])
+add_meme("out", out, min_images=1, max_images=1, keywords=["out"])
