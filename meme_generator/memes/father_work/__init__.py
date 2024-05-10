91eb83a6106cbc8feba58ca9bc1e4230951ad69a
reine-ishyanami <2402979195@qq.com>
2024-05-10 23:29:35 +0800

    feat: add meme `father_work`

---
+++ meme_generator/memes/father_work/__init__.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import List
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.exception import TextOverLength
+from meme_generator.utils import make_jpg_or_gif
+
+img_dir = Path(__file__).parent / "images"
+
+
+def father_work(images: List[BuildImage], texts: List[str], args):
+    frame = BuildImage.open(img_dir / "0.png").resize_width(500).convert("RGBA")
+    text = texts[0] if texts else "此处添加文字"
+    try:
+        frame.draw_text(
+            (195, frame.height - 110, frame.width - 10, frame.height - 20),
+            text,
+            fontsize=20,
+            min_fontsize=10,
+            max_fontsize=50,
+            fill="black",
+        )
+    except ValueError:
+        raise TextOverLength(text)
+
+    def make(img: BuildImage) -> BuildImage:
+        img = img.convert("RGBA").resize((230, 96), keep_ratio=True)
+        return frame.copy().paste(img, (252, 150), below=False)
+
+    return make_jpg_or_gif(images[0], make)
+
+
+add_meme(
+    "father_work",
+    father_work,
+    min_images=1,
+    max_images=1,
+    min_texts=0,
+    max_texts=1,
+    default_texts=["此处添加文字"],
+    keywords=["闭嘴", "我爸爸"],
+)
