ae91ccbe5cb7af03229c497793d69b824fe34480
meetwq <meetwq@gmail.com>
2024-05-16 11:14:01 +0800

    adjust

--- meme_generator/memes/fight_with_sunuo/__init__.py
+++ meme_generator/memes/fight_with_sunuo/__init__.py
@@ -12,7 +12,7 @@ def fight_with_sunuo(images: list[BuildImage], texts, args):
     frame = BuildImage.open(img_dir / "0.png")
 
     def make(img: BuildImage) -> BuildImage:
-        img = img.convert("RGBA").resize((565, 1630), keep_ratio=True)
+        img = img.convert("L").resize((565, 1630), keep_ratio=True)
         return frame.copy().paste(img, (0, 245), below=True)
 
     return make_jpg_or_gif(images[0], make)
