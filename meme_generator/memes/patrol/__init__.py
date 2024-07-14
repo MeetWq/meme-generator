f914845245a9fab3f6b524e8397f84fee6167e78
meetwq <meetwq@gmail.com>
2024-07-14 16:01:39 +0800

    rewrite patrol

--- meme_generator/memes/patrol/__init__.py
+++ meme_generator/memes/patrol/__init__.py
@@ -1,59 +1,47 @@
-from pydantic import Field
-
 from PIL.Image import Transpose
 from pil_utils import BuildImage
 
-from meme_generator import MemeArgsModel, MemeArgsParser, MemeArgsType, add_meme
+from meme_generator import add_meme
 from meme_generator.utils import FrameAlignPolicy, Maker, make_gif_or_combined_gif
 
-help = "是否将图片水平翻转"
 
-parser = MemeArgsParser(prefix_chars="-/")
-parser.add_argument("--horizontal", "/水平翻转", action="store_true", help=help)
+def patrol(images: list[BuildImage], texts, args):
+    img_w = 100
+    img_h = images[0].resize_width(img_w).height
+    frame_w = 300
+    frame_h = img_h + 30
+    frame = BuildImage.new("RGBA", (frame_w, frame_h))
 
+    def traj(x: float) -> float:
+        h = 15
+        w = (frame_w - img_w) / 4
+        k = h / w**2
+        if x < img_w / 2 or x > frame_w - img_w / 2:
+            return 0
+        elif x < frame_w / 2:
+            return h - k * (x - img_w / 2 - w) ** 2
+        else:
+            return h - k * (x - img_w / 2 - w * 3) ** 2
 
-class Model(MemeArgsModel):
-    horizontal: bool = Field(False, description=help)
+    frame_num = 30
+    dx = (frame_w - img_w) / (frame_num / 2 - 1)
 
-
-def patrol(images: list[BuildImage], texts, args: Model):
-    # fmt: off
-    locs = [
-        (54, 16), (52, 13), (49, 10), (46, 9), (40, 10),
-        (34, 13), (31, 16), (28, 13), (22, 10), (15, 10),
-        (7, 12), (3, 14), (0, 16),
-        (1, 16), (3, 13), (7, 10), (12, 10), (16, 9),
-        (22, 11), (25, 13), (28, 16), (32, 12), (37, 9),
-        (42, 7), (47, 7), (51, 9), (53, 11), (55, 14),
-        (54, 16)
-    ]
-    # fmt: on
     def maker(i: int) -> Maker:
         def make(img: BuildImage) -> BuildImage:
-            img = images[0].convert("RGBA").square()
-            if args.horizontal:
+            img = images[0].convert("RGBA").resize_width(img_w)
+            if i >= round(frame_num / 2):
+                x = frame_w - img_w - dx * (frame_num - i - 1)
                 img = img.transpose(Transpose.FLIP_LEFT_RIGHT)
-            if i >= 13:
-                img = img.transpose(Transpose.FLIP_LEFT_RIGHT)
-            frame = BuildImage.new("RGBA", (96, 57), "white")
-            x, y = locs[i]
-            frame.paste(img.resize_width(42), (x, y), alpha=True)
-            return frame
+            else:
+                x = frame_w - img_w - dx * i
+            y = frame_h - (traj(x + img_w / 2) + img_h)
+            return frame.copy().paste(img, (round(x), round(y)), alpha=True)
 
         return make
 
     return make_gif_or_combined_gif(
-        images[0], maker, 29, 0.05, FrameAlignPolicy.extend_loop
+        images[0], maker, frame_num, 0.04, FrameAlignPolicy.extend_loop
     )
 
 
-add_meme(
-    "patrol",
-    patrol,
-    min_images=1,
-    max_images=1,
-    args_type=MemeArgsType(
-        parser, Model, [Model(horizontal=False), Model(horizontal=True)]
-    ),
-    keywords=["巡逻"],
-)
+add_meme("patrol", patrol, min_images=1, max_images=1, keywords=["巡逻"])
