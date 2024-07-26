4c468869441d9ab7c2c0769ad1f2d07d07751dd4
電撃メロン <109346923+dkqdb66@users.noreply.github.com>
2024-07-26 17:39:39 +0800

    feat: add meme `play_together` (#118)
    * Create __init__.py
    
    * Create readme.md
    
    * Add files via upload
    
    * Delete meme_generator/memes/shanghao/images/readme.md
    
    * style: auto fix by pre-commit hooks
    
    * adjust
    
    * Update resource_list.json
    
    * rename shanghao to play_together
    
    * Update resource_list.json
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: meetwq <meetwq@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ meme_generator/memes/play_together/__init__.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.utils import FrameAlignPolicy, Maker, make_gif_or_combined_gif
+
+img_dir = Path(__file__).parent / "images"
+
+
+def play_together(images: list[BuildImage], texts, args):
+    params = (
+        36 * [(194, 204, 121, 108)]
+        + [
+            (192, 203, 125, 111),
+            (182, 200, 141, 126),
+            (161, 188, 178, 159),
+            (129, 171, 235, 209),
+            (98, 155, 290, 258),
+            (98, 155, 290, 258),
+            (58, 133, 361, 321),
+            (45, 126, 384, 342),
+            (45, 126, 384, 342),
+            (35, 121, 402, 358),
+            (27, 117, 415, 370),
+            (17, 111, 433, 386),
+            (14, 110, 439, 391),
+            (14, 110, 439, 391),
+            (11, 108, 444, 395),
+            (10, 108, 446, 397),
+        ]
+        + 8 * [(7, 106, 451, 402)]
+    )
+
+    def maker(i: int) -> Maker:
+        def make(img: BuildImage) -> BuildImage:
+            x, y, w, h = params[i]
+            screen = img.convert("RGBA").resize((w, h), keep_ratio=True)
+            frame = BuildImage.open(img_dir / f"{i}.png")
+            frame.paste(screen, (x, y), below=True)
+            return frame
+
+        return make
+
+    return make_gif_or_combined_gif(
+        images[0], maker, 60, 0.06, FrameAlignPolicy.extend_last
+    )
+
+
+add_meme(
+    "play_together", play_together, min_images=1, max_images=1, keywords=["一起玩"]
+)
