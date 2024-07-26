de911078abb303604bad66ec0a289315863ddac8
meetwq <meetwq@gmail.com>
2024-07-26 17:23:24 +0800

    adjust

--- meme_generator/memes/shanghao/__init__.py
+++ meme_generator/memes/shanghao/__init__.py
@@ -1,85 +1,49 @@
 from pathlib import Path
+
 from pil_utils import BuildImage
+
 from meme_generator import add_meme
 from meme_generator.utils import FrameAlignPolicy, Maker, make_gif_or_combined_gif
-
 
 img_dir = Path(__file__).parent / "images"
 
 
 def shanghao(images: list[BuildImage], texts, args):
-    width, height = BuildImage.open(img_dir / "0.png").size
-    rmin = 225
-    rmax = 825
-    r_list = (
-        36 * [rmin]
+    params = (
+        36 * [(194, 204, 121, 108)]
         + [
-            236,
-            260,
-            329,
-            434,
-            526,
-            526,
-            668,
-            716,
-            716,
-            742,
-            760,
-            791,
-            805,
-            805,
-            815,
-            822,
+            (192, 203, 125, 111),
+            (182, 200, 141, 126),
+            (161, 188, 178, 159),
+            (129, 171, 235, 209),
+            (98, 155, 290, 258),
+            (98, 155, 290, 258),
+            (58, 133, 361, 321),
+            (45, 126, 384, 342),
+            (45, 126, 384, 342),
+            (35, 121, 402, 358),
+            (27, 117, 415, 370),
+            (17, 111, 433, 386),
+            (14, 110, 439, 391),
+            (14, 110, 439, 391),
+            (11, 108, 444, 395),
+            (10, 108, 446, 397),
         ]
-        + 8 * [rmax]
-    )
-    loc0 = (356, 366)
-    loc1 = (19, 149)
-    loc_list = (
-        36 * [loc0]
-        + [
-            (354, 362),
-            (338, 352),
-            (300, 328),
-            (240, 290),
-            (179, 250),
-            (179, 250),
-            (105, 210),
-            (78, 191),
-            (78, 191),
-            (62, 180),
-            (55, 174),
-            (38, 158),
-            (30, 158),
-            (30, 158),
-            (24, 148),
-            (16, 149),
-        ]
-        + 8 * [loc1]
+        + 8 * [(7, 106, 451, 402)]
     )
 
     def maker(i: int) -> Maker:
         def make(img: BuildImage) -> BuildImage:
-            r = r_list[i]
-            loc = (
-                int(width * loc_list[i][0] / 1000),
-                int(height * loc_list[i][1] / 1000),
-            )
-            game = (
-                img.square()
-                .convert("RGBA")
-                .resize(
-                    (int(r * width / 1000), int(r * height / 1000)), keep_ratio=True
-                )
-            )
-            frame = BuildImage.open(img_dir / f"{i}.png").convert("RGBA")
-            frame.paste(game, loc, below=True)
+            x, y, w, h = params[i]
+            screen = img.convert("RGBA").resize((w, h), keep_ratio=True)
+            frame = BuildImage.open(img_dir / f"{i}.png")
+            frame.paste(screen, (x, y), below=True)
             return frame
 
         return make
 
     return make_gif_or_combined_gif(
-        images[0], maker, 60, 0.066, FrameAlignPolicy.extend_loop
+        images[0], maker, 60, 0.06, FrameAlignPolicy.extend_last
     )
 
 
