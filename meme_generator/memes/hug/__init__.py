eb9a816f9e055647cdcc5b56f6fdae4a1dcaf6ea
meetwq <meetwq@gmail.com>
2024-08-06 19:29:50 +0800

    adjust

--- meme_generator/memes/hug/__init__.py
+++ meme_generator/memes/hug/__init__.py
@@ -14,21 +14,21 @@ def hug(images: list[BuildImage], texts, args):
     user_head = images[1].convert("RGBA").circle().resize((105, 105))
     # fmt: off
     user_locs = [
-        (108, 15), (107, 14), (104, 16), (102, 14), (104, 15), (108, 15), (108, 15), (103, 16), (102, 15), (104, 14)
+        (108, 15), (107, 14), (104, 16), (102, 14), (104, 15),
+        (108, 15), (108, 15), (103, 16), (102, 15), (104, 14)
     ]
     self_locs = [
-        (78, 120), (115, 130), (0, 0), (110, 100), (80, 100), (75, 115), (105, 127), (0, 0), (110, 98), (80, 105)
-]
-    rotate_num = [
-        -48, -18, 0, 38, 31, -43, -22, 0, 34, 35
+        (78, 120), (115, 130), (0, 0), (110, 100), (80, 100),
+        (75, 115), (105, 127), (0, 0), (110, 98), (80, 105)
     ]
+    rotate_num = [-48, -18, 0, 38, 31, -43, -22, 0, 34, 35]
     # fmt: on
     frames: list[IMG] = []
     for i in range(10):
         frame = BuildImage.open(img_dir / f"{i}.png")
-        frame.paste(user_head, user_locs[i], alpha=True, below=True)
+        frame.paste(user_head, user_locs[i], below=True)
         img = self_head.rotate(rotate_num[i], expand=True)
-        frame.paste(img, self_locs[i], alpha=True, below=True)
+        frame.paste(img, self_locs[i], below=True)
         frames.append(frame.image)
     return save_gif(frames, 0.05)
 
