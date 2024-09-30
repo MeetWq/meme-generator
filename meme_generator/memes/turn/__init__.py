a88b97ff4ef6e10326adc99c64dfd21edeab3e99
meetwq <meetwq@gmail.com>
2024-09-30 15:00:04 +0800

    adjust `turn` background

--- meme_generator/memes/turn/__init__.py
+++ meme_generator/memes/turn/__init__.py
@@ -12,7 +12,7 @@ def turn(images: list[BuildImage], texts, args):
     img = images[0].convert("RGBA").circle()
     frames: list[IMG] = []
     for i in range(0, 360, 10):
-        frame = BuildImage.new("RGBA", (250, 250), "white")
+        frame = BuildImage.new("RGBA", (250, 250))
         frame.paste(img.rotate(i).resize((250, 250)), alpha=True)
         frames.append(frame.image)
     if random.randint(0, 1):
@@ -27,5 +27,5 @@ add_meme(
     max_images=1,
     keywords=["转"],
     date_created=datetime(2022, 1, 1),
-    date_modified=datetime(2023, 2, 14),
+    date_modified=datetime(2024, 9, 30),
 )
