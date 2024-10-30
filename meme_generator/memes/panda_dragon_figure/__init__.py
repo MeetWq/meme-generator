d6903371dadde3b5c54d761a93701a3dddb94aa5
meetwq <meetwq@gmail.com>
2024-10-30 19:36:49 +0800

    adjust

--- meme_generator/memes/panda_dragon_figure/__init__.py
+++ meme_generator/memes/panda_dragon_figure/__init__.py
@@ -1,50 +1,69 @@
-import re
+from datetime import datetime
 from pathlib import Path
 
 from pil_utils import BuildImage
+from pydantic import Field
 
-from meme_generator import add_meme
+from meme_generator import (
+    CommandShortcut,
+    MemeArgsModel,
+    MemeArgsType,
+    ParserArg,
+    ParserOption,
+    add_meme,
+)
 from meme_generator.exception import TextOverLength
 
 img_dir = Path(__file__).parent / "images"
 
+help_text = "龙图名字"
 
-def panda_dragon_figure(images, texts: list[str], args):
+
+class Model(MemeArgsModel):
+    name: str = Field("", description=help_text)
+
+
+args_type = MemeArgsType(
+    args_model=Model,
+    parser_options=[
+        ParserOption(
+            names=["-n", "--name"],
+            args=[ParserArg(name="name", value="str")],
+            help_text=help_text,
+        ),
+    ],
+)
+
+
+def panda_dragon_figure(images, texts: list[str], args: Model):
+    name = args.name or "责怪龙"
+    text = texts[0]
     frame = BuildImage.open(img_dir / "0.png")
-
-    text = texts[0].strip()
-    match = re.search(r"([^ ]*龙)", text)
-    if match:
-        dragon_text = match.group(1)
-        text = text.replace(dragon_text, "")
-    else:
-        dragon_text = "责怪龙"
 
     try:
         frame.draw_text(
-            (140, 460),
-            dragon_text,
-            allow_wrap=True,
-            lines_align="center",
-            fontsize=60,
+            (0, 470, 470, 550),
+            name,
+            allow_wrap=False,
+            max_fontsize=60,
+            min_fontsize=20,
             fill=(255, 255, 255),
         )
     except ValueError:
-        raise TextOverLength(dragon_text)
+        raise TextOverLength(name)
 
-    if text:
-        try:
-            frame.draw_text(
-                (0, 0, 550, 120),
-                text,
-                allow_wrap=True,
-                lines_align="center",
-                max_fontsize=100,
-                min_fontsize=20,
-                fill=(0, 0, 0),
-            )
-        except ValueError:
-            raise TextOverLength(text)
+    try:
+        frame.draw_text(
+            (0, 0, 550, 120),
+            text,
+            allow_wrap=True,
+            lines_align="center",
+            max_fontsize=100,
+            min_fontsize=20,
+            fill=(0, 0, 0),
+        )
+    except ValueError:
+        raise TextOverLength(text)
 
     return frame.save_png()
 
@@ -54,5 +73,16 @@ add_meme(
     panda_dragon_figure,
     min_texts=1,
     max_texts=1,
+    default_texts=["我要玩原神"],
+    args_type=args_type,
     keywords=["熊猫龙图"],
+    shortcuts=[
+        CommandShortcut(
+            key=r"(?P<name>\S{1,10})龙[\s:：]+(?P<text>\S+)",
+            args=["--name", "{name}龙", "{text}"],
+            humanized="xx龙：xx",
+        )
+    ],
+    date_created=datetime(2024, 10, 30),
+    date_modified=datetime(2024, 10, 30),
 )
