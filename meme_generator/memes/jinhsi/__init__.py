8782c6f1a896001bc286edd9c041eebc95694ed5
Yuki-01 <56385972+Yuki-01@users.noreply.github.com>
2024-12-09 23:29:53 +0800

    Update __init__.py

--- meme_generator/memes/jinhsi/__init__.py
+++ meme_generator/memes/jinhsi/__init__.py
@@ -18,7 +18,7 @@ from meme_generator.tags import MemeTags
 img_dir = Path(__file__).parent / "images"
 
 
-help_text = "图片编号，范围为 1~12"
+help_text = "图片编号，范围为 1~13"
 
 
 class Model(MemeArgsModel):
@@ -39,7 +39,7 @@ args_type = MemeArgsType(
 
 def jinhsi(images, texts: list[str], args: Model):
     text = texts[0]
-    total_num = 12
+    total_num = 13
     if args.number == 0:
         num = random.randint(1, total_num)
     elif 1 <= args.number <= total_num:
@@ -47,7 +47,7 @@ def jinhsi(images, texts: list[str], args: Model):
     else:
         raise MemeFeedback(f"图片编号错误，请选择 1~{total_num}")
 
-    paddings = (55, 43, 50, 36, 40, 33, 36, 38, 33, 46, 26, 33)
+    paddings = (55, 43, 50, 36, 40, 33, 36, 38, 33, 46, 26, 33, 28)
     frame = BuildImage.open(img_dir / f"{num:02d}.png")
     padding = paddings[num - 1]
 
