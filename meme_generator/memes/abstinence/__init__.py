fddd800e82354211f641c9ea453ce3a414d826e1
meetwq <meetwq@gmail.com>
2024-12-14 12:27:53 +0800

    adjust

--- meme_generator/memes/abstinence/__init__.py
+++ meme_generator/memes/abstinence/__init__.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pathlib import Path
 
 import dateparser
-from pil_utils import BuildImage, Text2Image
+from pil_utils import BuildImage
 from pydantic import Field
 
 from meme_generator import (
@@ -45,18 +45,25 @@ def abstinence(images: list[BuildImage], texts: list[str], args: Model):
         time = parsed_time
     name = args.name or (args.user_infos[-1].name if args.user_infos else "")
 
-    img = images[0].convert("RGBA").resize((300, 300), keep_ratio=True, inside=True, bg_color="white")
-
-    frame = BuildImage.open(img_dir / "Base.png").convert("RGBA")
-
-    frame.paste(img, (80, 400))
-    frame.draw_bbcode_text(
-        (150, 650, 760, 800),
-        text=f"戒导人：[u]{name}[/u]",
-        max_fontsize=20,
-        min_fontsize=10,
-        halign="center",
+    img = (
+        images[0]
+        .convert("RGBA")
+        .resize((300, 300), keep_ratio=True, inside=True, bg_color="white")
     )
+
+    frame = BuildImage.open(img_dir / "base.png").convert("RGBA")
+
+    try:
+        frame.paste(img, (80, 400))
+        frame.draw_bbcode_text(
+            (150, 650, 760, 800),
+            text=f"戒导人：[u] {name} [/u]",
+            max_fontsize=20,
+            min_fontsize=10,
+            halign="center",
+        )
+    except ValueError:
+        raise TextOverLength(name)
     frame.draw_bbcode_text(
         (150, 750, 760, 800),
         text=f"[u] {time.year} [/u]年[u] {time.month} [/u]月[u] {time.day} [/u]日",
@@ -64,14 +71,10 @@ def abstinence(images: list[BuildImage], texts: list[str], args: Model):
         min_fontsize=10,
         halign="center",
     )
-    try:
-        stamp = BuildImage.open(img_dir / "Stamp.png").convert("RGBA")
-        stamp = stamp.resize((200, 200))  # 替换 width, height 为合适的大小
-        frame.paste(stamp, (350, 650), alpha=True)  # 替换 x, y  为合适的坐标, 保持透明度
-    except FileNotFoundError:
-        print("stamp.png 文件未找到!")
-    except Exception as e:
-        print(f"加载 stamp.png 失败: {e}")
+
+    stamp = BuildImage.open(img_dir / "stamp.png").convert("RGBA")
+    stamp = stamp.resize((200, 200))
+    frame.paste(stamp, (350, 650), alpha=True)
 
     return frame.save_jpg()
 
@@ -82,7 +85,7 @@ add_meme(
     min_images=1,
     max_images=1,
     min_texts=0,
-    max_texts=1,
+    max_texts=0,
     args_type=args_type,
     keywords=["戒导"],
     date_created=datetime(2024, 12, 13),
